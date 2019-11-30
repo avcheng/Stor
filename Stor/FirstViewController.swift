977caeb58ca69addcd8f0a492bcd415d8dc077f3
rivel100 <rivel100@gmail.com>
2019-11-29 23:29:15 -0500

    Initial Commit

---
+++ Stor/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Stor
+//
+//  Created by Alex Cheng on 11/29/19.
+//  Copyright © 2019 Alex Cheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
